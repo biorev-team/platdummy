55e187be453c49e981c3596595e7d430cc56fee8
gulshan31 <k2caher@gmail.com>
2020-01-15 15:03:05 +0530

    #API built for reading the data

---
+++ api/builder/read.php
@@ -0,0 +1,62 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+// includes files here  
+include_once '../objects/builder.php';
+include_once '../include/dbConnect.php';
+
+$dbConn = new DB_Connect();
+$conn = $dbConn->connect();
+//$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+//$uri = explode( '/', $uri );
+//if ($uri[1] !== 'platdummy') {
+//    header("HTTP/1.1 404 Not Found");
+//    exit();
+//}
+
+$builder = new Builder($conn);
+$stmt = $builder->read();
+$count = $stmt->num_rows;
+if($count > 0){
+    $builder = array();
+    $builder["method"]=$_SERVER["REQUEST_METHOD"];
+    $builder["response"]=http_response_code(200);
+    $builder["success"]=true;
+    $builder["data"] = array();
+    $builder["count"] = $count;
+
+    while ($row = mysqli_fetch_array($stmt)){
+
+        extract($row);
+
+        $p  = array(
+              "builder_id" => $builder_id,
+              "builder_name" => $builder_name,
+              "contact" =>$contact,
+              "status" =>$status,
+              "area" => $area_name
+        );
+
+        array_push($builder["data"], $p);
+    }
+    
+    echo json_encode($builder,JSON_PRETTY_PRINT);
+}
+
+else {
+// set response code - 404 Not found
+    $error_arr = array();   
+    $error_arr["success"] = false;
+    $error_arr["response"] =http_response_code(404);
+    $error_arr["data"] = array();
+    echo json_encode(
+        $error_arr,JSON_PRETTY_PRINT
+    );
+}
+
+
+?>
+    
---
+++ api/objects/builder.php
@@ -0,0 +1,93 @@
+<?php 
+ class Builder{
+ // connection instance    
+private $connection;
+// store builder id     
+public $builder_id;
+// store builder name     
+public $builder_name; 
+// store buildr status is active or passive?     
+public $status;
+// builder contact info     
+public $contact;     
+ // builder area
+public $area_name;
+// updated at
+public $updated_at;
+// created at
+public $created_at;     
+     function __construct($conn){
+        $this->connection = $conn;
+    }      
+ //function which will read the builders available into the database      
+public function read(){
+    // select all query
+    $query = "SELECT builders_info.builder_id, builders_info.builder_name, builders_info.contact, areas_info.area_name,builders_info.status FROM builders_info INNER JOIN areas_info ON builders_info.builder_id = areas_info.builder_id";
+    // prepare query statement
+    $resultSet = mysqli_query($this->connection, $query);
+    return $resultSet;
+}  
+// read single builder information
+     public function read_single(){
+         
+     $query = "SELECT * FROM builders_info WHERE builder_id = '$this->builder_id' ";      
+     $rs = mysqli_query($this->connection,$query);
+     $row = mysqli_fetch_array($rs);
+     $this->builder_name = $row['builder_name'];
+     $this->status = $row['status'];
+     $this->contact = $row['contact']; 
+     $this->area_name = $row['area_name'];     
+         
+              
+         // prepare statement
+//        $stmt = $this->connection->prepare($query);
+//        $stmt->bind_param("i",$this->builder_id);
+//        $stmt->execute();
+//        $result = $stmt->get_result()->fetch_assoc();
+//         return $result;
+
+//          while($row = mysql_fetch_assoc($stmt)){
+//         $this->builder_name = $row['builder_name'];
+//         $this->status = $row['status'];
+//         $this->contact = $row['contact'];
+//          }
+         
+     }
+     // CREAT NEW BUILDER
+     public function create_builder(){
+         
+         // query to insert record
+    $query = "INSERT INTO
+                builders_info(builder_name,status,updated_at,created_at,contact) VALUES(?,?,?,?,?) 
+            ";
+ 
+    // prepare query
+    $stmt = $this->connection->prepare($query);
+ 
+    // sanitize
+    $this->builder_name=htmlspecialchars(strip_tags($this->builder_name));
+    $this->status=htmlspecialchars(strip_tags($this->status));
+    $this->updated_at=htmlspecialchars(strip_tags($this->updated_at)); 
+    $this->created_at=htmlspecialchars(strip_tags($this->created_at));  
+    $this->contact=htmlspecialchars(strip_tags($this->contact));     
+ 
+    // bind values
+    $stmt->bind_param("sssss", $this->builder_name,$this->status,$this->updated_at,$this->created_at,$this->contact);
+    
+ 
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+         
+         
+         
+         
+         
+     }
+     
+ }
+
+?>
