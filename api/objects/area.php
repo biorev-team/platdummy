4ccb94745cb5e863cbc341b63f135a6ccadcd9f9
gulshan31 <k2caher@gmail.com>
2020-01-20 12:57:18 +0530

    #added functionality for add new area

--- api/objects/area.php
+++ api/objects/area.php
@@ -4,6 +4,7 @@ class Area{
     public $area_id;
     public $builder_id;
     public $area_name;
+    public $area_address;
     public $primary_image;
     public $images;
     public $status;
@@ -22,8 +23,38 @@ class Area{
         $resultSet = mysqli_query($this->connection,$query);
         return $resultSet;
     }
+    // Add new area to the database
     
+    public function create(){
+        // Check if the area name already exist in the data base.
+        // select query
+        $this->area_name = htmlspecialchars(strip_tags($this->area_name));
+        $this->area_address = htmlspecialchars(strip_tags($this->area_address));
+        $selectQry = "SELECT area_name FROM areas_info WHERE area_name='$this->area_name'";
+        $res = mysqli_query($this->connection,$selectQry);
+        if($res->num_rows>0){
+            
+            return false;
+        }
+        else{
+            
+             // Query to insert    
+        $query = "INSERT INTO areas_info(area_name,area_address,primary_image) VALUES (?,?,?)";
+        
+        $stmt = $this->connection->prepare($query);
     
+        // Bind the parameters
+        $stmt->bind_param("sss",$this->area_name,$this->area_address,$this->primary_image);
+        if($stmt->execute()){
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+   
+        
+    }
     
     
 }
